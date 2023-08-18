553a208bfac46aef3edf128472e77ff1e41a68a4
Indraja Naik <indrajanaik@Indrajas-MacBook-Air.local>
2023-08-18 23:26:15 +0530

    01

---
+++ 01-basic-react-project/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World From Create React App</h1>
+      <Chai />
+    </>
+    
+  );
+}
+
+export default App;
---
+++ vite-project-react/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    // <h1>Hello World From React Vite Project!</h1>
+    <> 
+      <Chai />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
